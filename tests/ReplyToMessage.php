f9d0750057d622ebb70376860cdec8f574de6499
Abbas Shakiba <Shakiba_9@yahoo.com>
2017-06-10 11:24:32 +0430

    Improve ReplyToMessage example

--- tests/ReplyToMessage.php
+++ tests/ReplyToMessage.php
@@ -3,13 +3,11 @@
 require_once '../vendor/autoload.php';
 
 
-$token = 'TOKEN';
+$token = 'Token';
 $t = new Telegram($token);
 $message = new \Shakibonline\Message($t->getData());
 
-echo 'Message Text: '. $message->ReplyToMessage()->Text() . PHP_EOL;
-echo 'Message Caption: '. $message->ReplyToMessage()->Caption() . PHP_EOL;
-echo 'Message Caption: '. $message->ReplyToMessage()->ForwardFromChat()->ID() . PHP_EOL;
-echo 'Message Caption: '. $message->ReplyToMessage()->ForwardFromChat()->UserName() . PHP_EOL;
-echo 'Message Caption: '. $message->ReplyToMessage()->ForwardFromChat()->Title() . PHP_EOL;
-
+echo 'Reply To Message Caption: '.                      $message->ReplyToMessage()->Caption() . PHP_EOL;
+echo 'Reply To Message Forward From Chat ID: '.         $message->ReplyToMessage()->ForwardFromChat()->ID() . PHP_EOL;
+echo 'Reply To Message Forward From Chat Title: '.      $message->ReplyToMessage()->ForwardFromChat()->Title() . PHP_EOL;
+echo 'Reply To Message Forward From Chat Username: '.   $message->ReplyToMessage()->ForwardFromChat()->UserName() . PHP_EOL;
